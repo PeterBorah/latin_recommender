46223f66eadefc02a4fce5c2125d6b4bc7551395
Peter Borah <peterborah@gmail.com>
2020-08-05 17:02:25 -0700

    Clean up word list and play with lemmatizer

--- latin_recommender.py
+++ latin_recommender.py
@@ -1,4 +1,31 @@
+from cltk.corpus.utils.importer import CorpusImporter
+from cltk.corpus.readers import get_corpus_reader
+from cltk.stem.latin.declension import CollatinusDecliner
+from cltk.stem.latin.j_v import JVReplacer
+from cltk.lemmatize.latin.backoff import BackoffLatinLemmatizer
+
+j = JVReplacer()
+
 with open("lingua_latina_vocab.txt") as f:
-    words = [line.split()[0].replace('-', '') for line in f]
-    print(words)
-    print(len(words))
+    words = [line.split()[0].
+            replace('-', '').
+            replace('ā', 'a').
+            replace('ē', 'e').
+            replace('ī', 'i').
+            replace('ō', 'o').
+            replace('ū', 'u')
+            for line in f]
+    # print(words)
+    # print(len(words))
+
+corpus_importer = CorpusImporter('latin')
+# print(corpus_importer.list_corpora)
+
+corpus_importer.import_corpus('latin_models_cltk')
+
+
+lemmatizer = BackoffLatinLemmatizer()
+lemmas = lemmatizer.lemmatize(words)
+
+decliner = CollatinusDecliner()
+print(decliner.decline(lemmas[40][1], flatten=True))
